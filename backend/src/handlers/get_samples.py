0210ceb42b2aaf9332010b71607ef26063e65e9b
gustblima <gustavolima320@gmail.com>
2019-12-10 03:15:36 -0300

    Remove spotify calls from backend

--- backend/src/handlers/get_samples.py
+++ backend/src/handlers/get_samples.py
@@ -1,12 +1,14 @@
 import json
-from backend.src.whosampled_scrape import create_samples_list
+from whosampled_scrape import WhoSampledScrape
 
 def handler(event, context):
 
     query_params = json.loads(event)['queryStringParameters']
     tracks = query_params['tracks']
+    
     if tracks:
-        result = list(create_samples_list(tracks.split(',')))
+        scrape = WhoSampledScrape()
+        result = list(scrape.create_samples_list(tracks.split(',')))
         return {
             "statusCode": 200,
             "body": json.dumps(result)
