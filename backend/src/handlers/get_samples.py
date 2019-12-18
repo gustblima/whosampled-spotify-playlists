09cbb2414bd3430c26a611c9d3b4ac72fdb4193e
= <gustavolima320@gmail.com>
2019-12-18 03:18:15 -0300

    Fix cors

--- backend/src/handlers/get_samples.py
+++ backend/src/handlers/get_samples.py
@@ -1,23 +1,28 @@
 import json
 from src.whosampled_scrape import WhoSampledScrape
-
+max_tracks_allowed_per_request = 5
 def handler(event, context):
     print(event)
     query_params = event['queryStringParameters']
     
     if query_params and "tracks" in query_params:
         tracks = query_params['tracks'].split(';')
-        scrape = WhoSampledScrape()
-        samples = []
-        for track in tracks:
-            samples.append(scrape.get_samples_from_track_search(track))
+        if len(tracks) < max_tracks_allowed_per_request:
+            scrape = WhoSampledScrape()
+            samples = []
+            for track in tracks:
+                samples.append(scrape.get_samples_from_track_search(track))
 
-        return response(200, json.dumps(samples))
+            return response(200, json.dumps(samples))
 
     return response(500)
 
 def response(code, body = None):
     return {
         "statusCode": code,
+        "headers": {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': True,
+        },
         "body": body
     }
